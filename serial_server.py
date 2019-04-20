6418712f20f629ae386ad9fb766e23a690124856
megaprokoli <you@example.com>
2019-04-20 07:42:39 -0700

    text drawer test

--- serial_server.py
+++ serial_server.py
@@ -6,9 +6,10 @@ from hardware.oled import Oled
 
 
 oled = Oled(drawer=TextDrawer("started"))
-
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
 server.bind(("127.0.0.1", 5300))
+oled.draw()
 
 print("running")
 
